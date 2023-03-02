73a1c54d848254e578568340e00fced156148bee
Pavel Ivanov <grammka@gmail.com>
2023-03-02 11:42:41 +0100

    add description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Dune Pro Wizards</title>
-        <meta name="description" content="" />
+        <meta name="description" content="Upgrade your Dune Analytics experience with Dune Pro Wizards! This must-have Chrome extension makes the query editor bigger, improves styles, and lets you view results in a separate modal window." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
