02de20a892c32371e1f6ac96f71fe9e9f99ef995
Pavel Ivanov <grammka@gmail.com>
2023-03-02 13:22:40 +0100

    add twitter og

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-const title = 'Dune Pro Wizards'
-const description = 'Upgrade your Dune Analytics experience with Dune Pro Wizards! This must-have Chrome extension makes the query editor bigger, improves styles, and lets you view results in a separate modal window.'
 
 const GTMHead = () => (
   <script
@@ -30,6 +28,10 @@ const GTMBody = () => (
   </noscript>
 )
 
+const title = 'Dune Pro Wizards'
+const description = 'Upgrade your Dune Analytics experience with Dune Pro Wizards! This must-have Chrome extension makes the query editor bigger, improves styles, and lets you view results in a separate modal window.'
+const ogImage = 'https://dunewizards.xyz/images/og.jpg'
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -42,7 +44,11 @@ export default function Document() {
         <meta name="og:url" content="https://dunewizards.xyz/" />
         <meta name="og:title" content={title} />
         <meta name="og:description" content={description} />
-        <meta name="og:image" content="https://dunewizards.xyz/images/og.jpg" />
+        <meta name="og:image" content={ogImage} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
