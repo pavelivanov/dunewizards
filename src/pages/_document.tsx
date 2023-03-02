3d7acd4cd71f4feefe3232cf446525b0c8fe6e64
Pavel Ivanov <grammka@gmail.com>
2023-03-02 11:47:42 +0100

    add opengraph image, meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const title = 'Dune Pro Wizards'
+const description = 'Upgrade your Dune Analytics experience with Dune Pro Wizards! This must-have Chrome extension makes the query editor bigger, improves styles, and lets you view results in a separate modal window.'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Dune Pro Wizards</title>
-        <meta name="description" content="Upgrade your Dune Analytics experience with Dune Pro Wizards! This must-have Chrome extension makes the query editor bigger, improves styles, and lets you view results in a separate modal window." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="description" content={description} />
+        <meta name="og:url" content="https://dunewizards.xyz/" />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content="https://dunewizards.xyz/images/og.jpg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
